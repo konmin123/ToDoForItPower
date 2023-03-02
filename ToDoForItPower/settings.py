43de9755ef2dc1f2b9fff188a58cd983554020d7
MindlinKS@yandex.ru <MindlinKS@yandex.ru>
2023-03-02 22:34:28 +0300

    add pagination

--- ToDoForItPower/settings.py
+++ ToDoForItPower/settings.py
@@ -86,6 +86,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 1
 }
 
 
