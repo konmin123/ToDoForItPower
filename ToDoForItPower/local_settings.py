29acce314a555b1c9ca0b5527b44e845abec4d45
MindlinKS@yandex.ru <MindlinKS@yandex.ru>
2023-03-03 10:37:17 +0300

    add local_settings

---
+++ ToDoForItPower/local_settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = [
+    "debug_toolbar",
+]
+
+MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+SERVER_VERSION = "0.0.1"
